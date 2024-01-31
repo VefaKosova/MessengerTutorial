17f976baf5042e07053253dc6a3991ad99352eff
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-01-31 14:21:57 +0300

    Initial Commit

---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Vefa Kosova on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
