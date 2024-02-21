d67bcab60e7f415d22c992dcbf500666cf850531
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-21 13:20:37 +0300

    -firebase added

---
+++ MessengerTutorial/Core/Chat/ChatMessageCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessageCell.swift
+//  MessengerTutorial
+//
+//  Created by Vefa Kosova on 21.02.2024.
+//
+
+import SwiftUI
+
+struct ChatMessageCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatMessageCell()
+}
---
+++ MessengerTutorial/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  MessengerTutorial
+//
+//  Created by Vefa Kosova on 20.02.2024.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
