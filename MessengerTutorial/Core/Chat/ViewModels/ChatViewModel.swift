4f74831e5197230f5e5fe4880a3f6f99001a859a
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-27 15:38:23 +0300

    Fetching and Displaying Messages - Completion Handlers

--- MessengerTutorial/Core/Chat/ViewModels/ChatViewModel.swift
+++ MessengerTutorial/Core/Chat/ViewModels/ChatViewModel.swift
@@ -9,10 +9,18 @@ import Foundation
 
 class ChatViewModel: ObservableObject {
     @Published var messageText = ""
+    @Published var messages = [Message]()
     let user: User
     
     init(user: User) {
         self.user = user
+        observeMessages()
+    }
+    
+    func observeMessages() {
+        MessageService.observeMessage(chatPartner: user) { messages in
+            self.messages.append(contentsOf: messages)
+        }
     }
     
     func sendMessage() {
