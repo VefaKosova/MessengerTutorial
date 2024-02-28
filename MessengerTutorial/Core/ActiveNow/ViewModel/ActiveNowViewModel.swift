7b8680415517025c347ebd5db7b3059a1788afcb
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-28 19:04:15 +0300

    Active Now View Implementation

---
+++ MessengerTutorial/Core/ActiveNow/ViewModel/ActiveNowViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ActiveNowViewModel.swift
+//  MessengerTutorial
+//
+//  Created by Vefa Kosova on 28.02.2024.
+//
+
+import Foundation
---
+++ MessengerTutorial/Routing/Route.swift
@@ -0,0 +1,8 @@
+//
+//  Route.swift
+//  MessengerTutorial
+//
+//  Created by Vefa Kosova on 28.02.2024.
+//
+
+import Foundation
